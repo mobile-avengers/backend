a84c964a458b5093c23331b0f0adf1dbad366833
3ilib0ba <evonniy@mail.ru>
2023-05-17 15:31:22 +0300

    predata.sql updated to make first sql objects(roles, test users, product types)

--- src/main/resources/database/predata.sql
+++ src/main/resources/database/predata.sql
@@ -2,6 +2,10 @@
 insert into role values (1, 'USER');
 insert into role values (2, 'ADMIN');
 
+-- USERS
+insert into users values (DEFAULT, 'chebyrashka@mail.ru', '4321', 2);
+insert into users values (DEFAULT, 'test_user@gmail.ru', '1111', 1);
+
 -- PRODUCTS TYPES
 insert into product_type values (1, 'clothes');
 insert into product_type values (2, 'shoes');
