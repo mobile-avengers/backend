60388cd56cb636c3ddfce407ae8fcd4881f50e7f
3ilib0ba <evonniy@mail.ru>
2023-05-22 21:41:42 +0300

    only one condition

--- src/main/kotlin/mobile/avengers/backend/enums/OrderConditions.kt
+++ src/main/kotlin/mobile/avengers/backend/enums/OrderConditions.kt
@@ -3,7 +3,10 @@ package mobile.avengers.backend.enums
 enum class OrderConditions(val status: String) {
     NEW("new"),
     CANCELED("canceled"),
+    PAYED("payed"),
     CONFIRMED("confirmed"),
+    BOUGHT_IN_SHOP("boughtInShop"), // выкуплен
+    SENT("sent"), // отправлен
     IN_DELIVERY("inDlivery"),
-    FINISHED("finished"),
+    CLIENT_RECEIVED("clientReceived"),
 }
